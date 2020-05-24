3280becde58049c120237748c18d609dd508d5f7
jhair gutierrez <jhgutsol@jhairs-MacBook-Air.local>
2020-05-24 01:01:39 -0500

    Get more news

--- scrapper/web_scrapper.py
+++ scrapper/web_scrapper.py
@@ -10,13 +10,19 @@ class WebScrapper:
 
     def get_final_data(self) -> list:
         if self.source == 'excelsior':
-            data = urllib.request.urlopen(
-                'https://www.excelsior.com.mx/ultima-hora').read().decode()
-            soup = BeautifulSoup(data, 'html.parser')
-            titles = soup.find_all('span', class_='ultima-hora-title')
-            descriptions = soup.find_all('span', class_='ultima-hora-summary')
-            anchors = soup.select(
-                'div.widget-content2.ultima-hora-content-wrapper > ul > li > a')
+            page = 0
+            titles = []
+            descriptions = []
+            anchors = []
+            while page < 5:
+                data = urllib.request.urlopen(
+                f'https://www.excelsior.com.mx/ultima-hora?page={page}').read().decode()
+                soup = BeautifulSoup(data, 'html.parser')
+                titles += soup.find_all('span', class_='ultima-hora-title')
+                descriptions += soup.find_all('span', class_='ultima-hora-summary')
+                anchors += soup.select(
+                    'div.widget-content2.ultima-hora-content-wrapper > ul > li > a')
+                page += 1
             if len(titles) == len(descriptions) and len(titles) == len(anchors):
                 for i in range(len(titles)):
                     self.final_array.append({
@@ -41,12 +47,18 @@ class WebScrapper:
                 return self.final_array
             return []
         elif self.source == '20minutos':
-            data = urllib.request.urlopen(
-                'https://www.20minutos.com.mx/minuteca/ciudad-de-mexico/').read().decode()
-            soup = BeautifulSoup(data, 'html.parser')
-            titles = soup.select('div.media-content > header > h1 > a')
-            descriptions = soup.find_all('div', class_="media-intro")
-            anchors = soup.select('div.media-content > header > h1 > a')
+            page = 1
+            titles = []
+            descriptions = []
+            anchors = []
+            while page < 5:
+                data = urllib.request.urlopen(
+                f'https://www.20minutos.com.mx/minuteca/ciudad-de-mexico/{page}/').read().decode()
+                soup = BeautifulSoup(data, 'html.parser')
+                titles += soup.select('div.media-content > header > h1 > a')
+                descriptions += soup.find_all('div', class_="media-intro")
+                anchors += soup.select('div.media-content > header > h1 > a')
+                page += 1
             if len(titles) == len(descriptions) and len(titles) == len(anchors):
                 for i in range(len(titles)):
                     self.final_array.append({
